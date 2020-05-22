0d3336cc8cb76c3cd19fb711815d5fe887f97aaf
stickler-ci <support@stickler-ci.com>
2020-05-22 09:46:09 +0000

    Fixing style errors.

--- test_ex1.py
+++ test_ex1.py
@@ -7,8 +7,7 @@ Created on Mon May 11 16:39:38 2020
 
 from ex1 import max_num
 
-def test_ex1 ():
-    #always diffirent number
-    assert max_num (5, 7, 9)==8
-    
-    
+
+def test_ex1():
+    # always diffirent number
+    assert max_num(5, 7, 9) == 8
